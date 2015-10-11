0bd54df7a49feb68f8929fcb1a97bbb88894d2a8
HandomX <chechel_mk@hotmail.com>
2015-10-11 12:23:30 -0300

    ajustes na formataçao dos dados que sao recebidos e enviados do cliente

---
+++ WebServiceKitap.DB/Repositorios/Interfaces/IRepositorioLivros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServiceKitap.DB.Entidades;
+
+namespace WebServiceKitap.DB.Repositorios.Interfaces
+{
+    public interface IRepositorioLivros
+    {
+        Task<List<Livro>> LivrosAll();
+        bool LivroExiste(string isbn);
+        Task LivroAdd(Livro livro);
+
+    }
+}
---
+++ WebServiceKitap.DB/Repositorios/RepositorioLivrosDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServiceKitap.DB.Entidades;
+using WebServiceKitap.DB.Repositorios.Interfaces;
+
+namespace WebServiceKitap.DB.Repositorios
+{
+    public class RepositorioLivrosDB : IRepositorioLivros
+    {
+        KitapContextDB _KitapDB;
+        public RepositorioLivrosDB()
+        {
+            _KitapDB = new KitapContextDB();
+        }
+        public async Task LivroAdd(Livro livro)
+        {
+            _KitapDB.Livros.Add(livro);
+            await _KitapDB.SaveChangesAsync();
+        }
+
+        public bool LivroExiste(string isbn)
+        {
+            var livro = _KitapDB.Livros.Where(l => l.Isbn == isbn);
+            if (livro == null)
+                return false;
+            return true;
+        }
+
+        public async Task<List<Livro>> LivrosAll()
+        {
+            var livros = await _KitapDB.Livros.ToListAsync<Livro>();
+            return livros;
+        }
+    }
+}
